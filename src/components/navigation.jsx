0f5313d6d9cb1328b24fa1ca67b93592037ac3c7
malirahul <malirahul6425@gmail.com>
2024-07-17 09:39:23 +0530

    Fixed navigation

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,80 +1,87 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+
+const scrollToTop = () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
+
+const handleClick = (e, navigate) => {
+  e.preventDefault();
+  scrollToTop();
+  navigate("/");
+};
+
+const ScrollToTop = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    scrollToTop();
+  }, [location]);
+
+  return null;
+};
 
 export const Navigation = () => {
-	return (
-		<nav
-			id="menu"
-			className="navbar navbar-default navbar-fixed-top"
-		>
-			<div className="container">
-				<div className="navbar-header">
-					<button
-						type="button"
-						className="navbar-toggle collapsed"
-						data-toggle="collapse"
-						data-target="#bs-example-navbar-collapse-1"
-					>
-						<span className="sr-only">Toggle navigation</span>
-						<span className="icon-bar"></span>
-						<span className="icon-bar"></span>
-						<span className="icon-bar"></span>
-					</button>
-					<Link
-						to="/"
-						className="navbar-brand page-scroll"
-						style={{ color: 'blue' }}
-					>
-						ResourceDekho
-					</Link>
-				</div>
+  const navigate = useNavigate();
 
-				<div
-					className="collapse navbar-collapse"
-					id="bs-example-navbar-collapse-1"
-				>
-					<ul className="nav navbar-nav navbar-right">
-						<li>
-							<a
-								href="#features"
-								className="page-scroll"
-							>
-								Features
-							</a>
-						</li>
-						<li>
-							<a
-								href="#about"
-								className="page-scroll"
-							>
-								About
-							</a>
-						</li>
-						<li>
-							<a
-								href="#services"
-								className="page-scroll"
-							>
-								Services
-							</a>
-						</li>
-						<li>
-							<a
-								href="#pricing"
-								className="page-scroll"
-							>
-								Plan and Package
-							</a>
-						</li>
-						<li>
-							<a
-								href="#testimonials"
-								className="page-scroll"
-							>
-								Testimonials
-							</a>
-						</li>
-						{/* <li>
+  return (
+    <>
+      <ScrollToTop />
+      <nav id='menu' className='navbar navbar-default navbar-fixed-top'>
+        <div className='container'>
+          <div className='navbar-header'>
+            <button
+              type='button'
+              className='navbar-toggle collapsed'
+              data-toggle='collapse'
+              data-target='#bs-example-navbar-collapse-1'
+            >
+              <span className='sr-only'>Toggle navigation</span>
+              <span className='icon-bar'></span>
+              <span className='icon-bar'></span>
+              <span className='icon-bar'></span>
+            </button>
+            <Link
+              to='/'
+              className='navbar-brand page-scroll'
+              style={{ color: "blue" }}
+              onClick={(e) => handleClick(e, navigate)}
+            >
+              ResourceDekho
+            </Link>
+          </div>
+
+          <div
+            className='collapse navbar-collapse'
+            id='bs-example-navbar-collapse-1'
+          >
+            <ul className='nav navbar-nav navbar-right'>
+              <li>
+                <a href='#features' className='page-scroll'>
+                  Features
+                </a>
+              </li>
+              <li>
+                <a href='#about' className='page-scroll'>
+                  About
+                </a>
+              </li>
+              <li>
+                <a href='#services' className='page-scroll'>
+                  Services
+                </a>
+              </li>
+              <li>
+                <a href='#pricing' className='page-scroll'>
+                  Plan and Package
+                </a>
+              </li>
+              <li>
+                <a href='#testimonials' className='page-scroll'>
+                  Testimonials
+                </a>
+              </li>
+              {/* <li>
 							<a
 								href="#team"
 								className="page-scroll"
@@ -82,25 +89,20 @@ export const Navigation = () => {
 								Team
 							</a>
 						</li> */}
-						<li>
-							<a
-								href="#faq"
-								className="page-scroll"
-							>
-								Faq
-							</a>
-						</li>
-						<li>
-							<a
-								href="#contact"
-								className="page-scroll"
-							>
-								Contact
-							</a>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</nav>
-	);
+              <li>
+                <a href='#faq' className='page-scroll'>
+                  Faq
+                </a>
+              </li>
+              <li>
+                <a href='#contact' className='page-scroll'>
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 };
--- src/components/pages/navigation-page.js
+++ src/components/pages/navigation-page.js
@@ -1,105 +1,104 @@
-import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
 
 export const NavigationPage = () => {
-	const location = useLocation();
-	const isActivePage = (page) => {
-		return location.pathname === page ? 'active' : '';
-	};
+  const location = useLocation();
+  const isActivePage = (page) => {
+    return location.pathname === page ? "active" : "";
+  };
 
-	return (
-		<nav
-			id="menu"
-			className="navbar navbar-default navbar-fixed-top"
-		>
-			<div className="container">
-				<div className="navbar-header">
-					<button
-						type="button"
-						className="navbar-toggle collapsed"
-						data-toggle="collapse"
-						data-target="#bs-example-navbar-collapse-1"
-					>
-						<span className="sr-only">Toggle navigation</span>
-						<span className="icon-bar"></span>
-						<span className="icon-bar"></span>
-						<span className="icon-bar"></span>
-					</button>
-					<Link
-						to="/"
-						className="navbar-brand page-scroll"
-						style={{ color: 'blue' }}
-					>
-						ResourceDekho
-					</Link>
-				</div>
+  const handleClick = (e) => {
+    e.preventDefault();
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
 
-				<div
-					className="collapse navbar-collapse"
-					id="bs-example-navbar-collapse-1"
-				>
-					<ul className="nav navbar-nav navbar-right">
-						<li className={isActivePage('/')}>
-							<Link
-								to="/"
-								className="page-scroll"
-							>
-								Home
-							</Link>
-						</li>
-						<li className={isActivePage('/about_us')}>
-							<Link
-								to="/about_us"
-								className="page-scroll"
-								style={{
-									fontWeight: 'bold',
-									color: isActivePage('/about_us') ? 'black' : 'inherit',
-								}}
-							>
-								About
-							</Link>
-						</li>
-						<li className={isActivePage('/services')}>
-							<a
-								href="/services"
-								className="page-scroll"
-								style={{
-									fontWeight: 'bold',
-									color: isActivePage('/services') ? 'black' : 'inherit',
-								}}
-							>
-								Services
-							</a>
-						</li>
-						<li className={isActivePage('/why_resourcedekho')}>
-							<Link
-								to="/why_resourcedekho"
-								className="page-scroll"
-								style={{
-									fontWeight: 'bold',
-									color: isActivePage('/why_resourcedekho')
-										? 'black'
-										: 'inherit',
-								}}
-							>
-								Why ResourceDekho?
-							</Link>
-						</li>
-						<li className={isActivePage('/join_us')}>
-							<Link
-								to="/join_us"
-								className="page-scroll"
-								style={{
-									fontWeight: 'bold',
-									color: isActivePage('/join_us') ? 'black' : 'inherit',
-								}}
-							>
-								Join Us
-							</Link>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav id='menu' className='navbar navbar-default navbar-fixed-top'>
+      <div className='container'>
+        <div className='navbar-header'>
+          <button
+            type='button'
+            className='navbar-toggle collapsed'
+            data-toggle='collapse'
+            data-target='#bs-example-navbar-collapse-1'
+          >
+            <span className='sr-only'>Toggle navigation</span>
+            <span className='icon-bar'></span>
+            <span className='icon-bar'></span>
+            <span className='icon-bar'></span>
+          </button>
+          <Link
+            className='navbar-brand page-scroll'
+            style={{ color: "blue" }}
+            onClick={handleClick}
+          >
+            ResourceDekho
+          </Link>
+        </div>
+
+        <div
+          className='collapse navbar-collapse'
+          id='bs-example-navbar-collapse-1'
+        >
+          <ul className='nav navbar-nav navbar-right'>
+            <li className={isActivePage("/")}>
+              <Link to='/' className='page-scroll'>
+                Home
+              </Link>
+            </li>
+            <li className={isActivePage("/about_us")}>
+              <Link
+                to='/about_us'
+                className='page-scroll'
+                style={{
+                  fontWeight: "bold",
+                  color: isActivePage("/about_us") ? "black" : "inherit",
+                }}
+              >
+                About
+              </Link>
+            </li>
+            <li className={isActivePage("/services")}>
+              <a
+                href='/services'
+                className='page-scroll'
+                style={{
+                  fontWeight: "bold",
+                  color: isActivePage("/services") ? "black" : "inherit",
+                }}
+              >
+                Services
+              </a>
+            </li>
+            <li className={isActivePage("/why_resourcedekho")}>
+              <Link
+                to='/why_resourcedekho'
+                className='page-scroll'
+                style={{
+                  fontWeight: "bold",
+                  color: isActivePage("/why_resourcedekho")
+                    ? "black"
+                    : "inherit",
+                }}
+              >
+                Why ResourceDekho?
+              </Link>
+            </li>
+            <li className={isActivePage("/join_us")}>
+              <Link
+                to='/join_us'
+                className='page-scroll'
+                style={{
+                  fontWeight: "bold",
+                  color: isActivePage("/join_us") ? "black" : "inherit",
+                }}
+              >
+                Join Us
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
